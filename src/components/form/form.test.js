19c1246bb3b378e4ad65e51b3ca2d71e937fcb7f
Junyoung Son <junyoungson808@gmail.com>
2022-10-26 01:12:51 +0000

    update with tests

--- src/components/form/form.test.js
+++ src/components/form/form.test.js
@@ -1,14 +1,24 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Form from './index';
 
-describe('Testing the Form Component', () => {
-  it('should render the form', () => {
+describe('Testing the Form Component', () => {   // Testing if the data is loaded on the page
+  it('Should make a good API call', () => {
     let handleApiCall = jest.fn();
 
     render(<Form handleApiCall={handleApiCall} />);
-    let method = screen.getByText('GET');
-    expect(method).toBeInTheDocument();
+    let button = screen.getByText('GO!');
+    expect(button).toBeInTheDocument();             
+    fireEvent.click(button)                        // Fire the Event to see if the button worked
+    expect(handleApiCall).toHaveBeenCalled();      //handles the API Call
   });
-  
+  it('Should Error out when a bad api call', () => {
+    let handleBadApiCall = jest.fn(() => new Error('test'));
+
+    render(<Form handleApiCall={handleBadApiCall} />);
+    let button = screen.getByText('GO!');
+    expect(button).toBeInTheDocument();             
+    fireEvent.click(button)                        // Fire the Event to see if the button worked
+    expect(handleBadApiCall).toHaveBeenCalled();      //handles the API Call
+  });
 });
