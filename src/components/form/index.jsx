84d40fd2c9f3c4e0f8c55a8c5db3bff58b5da949
Junyoung Son <junyoungson808@gmail.com>
2022-10-27 02:35:19 +0000

    text Area included

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -6,12 +6,12 @@ const Form = (props) => {
 
   const [method, setMethod] = useState('');
   const [url, setURL] = useState('');
-
-
+  const [json, setJson] = useState('');
+ 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    props.handleApiCall({method, url});
+    props.handleApiCall({method, url, json});
   };
 
   return (
@@ -23,6 +23,8 @@ const Form = (props) => {
           <input onChange={(e) => setURL(e.target.value)} name='url' type='text' />
           <button type="submit">GO!</button>
         </label>
+        <textarea className='json-text-area' onChange={(e) => setJson(e.target.value)} type='text' placeholder='input JSON' />
+
         <label className="methods">
           <span data-testid="get-test" onClick={(e) => setMethod('GET')} id="get">GET</span>
           <span  id="post" onClick={(e) => setMethod('POST')}>POST</span>
